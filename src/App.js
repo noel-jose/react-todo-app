0c280c619da54eaf2ea790570a5fe650e2c18709
noel-jose <noeljosetp@gmail.com>
2021-09-10 19:43:53 +0530

    Added function to delete

--- src/App.js
+++ src/App.js
@@ -25,11 +25,17 @@ function App() {
           },
       ]
   );
+
+  // Delete task
+  const deleteTask = (id) =>{
+    console.log('delete',id);
+    setTasks(tasks.filter((task)=> task.id !== id));
+  }
  
   return (
     <div className="container">
       <Header/>
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask}/>) : ('No tasks')}
     </div>
   );
 }
